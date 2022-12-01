b0881451d56cd4789d00f50537365b3b768bdf37
Nick Brown <nmb@pop-os.localdomain>
2022-12-01 14:08:13 -0500

    Day 1 complete

--- aoc_01.c
+++ aoc_01.c
@@ -1,7 +1,86 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void ReplaceLowestValueInArrayInArray(int array[], int arraySize, int newValue)
+{
+    int lowestIndex = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+        // printf("Value at index %i: %i\n", i, array[i]);
+        // printf("Lowest Value: %i\n", array[lowestIndex]);
+
+        if(array[i] < array[lowestIndex])
+        {
+            lowestIndex = i;
+        }
+    }
+
+    // printf("Replacing [%i] with [%i]\n\n", array[lowestIndex], newValue);
+    array[lowestIndex] = newValue;
+    
+}
+
+void PrintHighestCalorieCount(FILE *input)
+{
+    int highestCal = 0, total = 0, lowest = 0;
+    int highestCals[3] = {0, 0, 0};
+    char line[1000];
+
+
+    int counter = 0;
+    while (fgets(line, sizeof(line), input) != NULL)
+    {
+        if(line[0] != '\n')
+        {
+            total += atoi(line);
+        }
+        else
+        {
+            if(total > highestCal)
+            {
+                highestCal = total;
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                if(total > highestCals[i])
+                {
+                    // printf("Total: %i\n" , total);
+                    ReplaceLowestValueInArray(highestCals, 3, total);
+                    break;
+                }
+            }
+
+            total = 0;
+        }
+
+        counter++;
+    }
+    
+    // This is the total calorie count for the elf carrying the most calories
+    printf("Highest 1: %i\n", highestCal); //69281
+
+    for (int i = 1; i < 3; i++)
+    {
+        highestCal += highestCals[i];
+        printf("Highest %i: %i\n", i+1, highestCals[i]);
+    }
+    
+    // This is the total calorie count for the top 3 elves
+    printf("Sum of 3 Highest: %i\n", highestCal); // 201524 = 69281 + 67653 + 64590
+}
 
 int main(void)
 {
-    printf("Getting ready for Advent of Code 2022");
-    return 0;
+    FILE *input = fopen("aoc_01_input.txt", "r");
+    if(input == NULL) 
+    {
+        printf("Error. File could not be opened\n");
+        exit(-1);
+    }
+
+    PrintHighestCalorieCount(input);
+
+    fclose(input);
+    exit(0);
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-Teaching myself C. Wish me luck...
+# AoC 22
+
+I had the (possibly dumb) idea of using this year's Advent of Code as an opportunity to teach myself C. I'm hoping at least some of my C# knowledge will translate.
+
+## Day 1
+- Reading a file line-by-line. This isn't too hard, but not sure if I did it in the most efficient way. 
+- I actually got the correct answer before my code was completely working for Part 2. I wasn't doing any sorting when checking the highest value in the array. This meant I wasn't actually replacing the lowest value, just the first value that was lower than the new value. The input data was written such that the sorting happened automatically and my code returned the correct value even though the logic was wrong. I tested with a few changes to the dataset and everything seems to be working correctly now.
